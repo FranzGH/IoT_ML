ad732850d45a92e18e7d0c18c1f175e36ca675e1
Francesco Bellotti <bellotti@elioslab.local>
2020-03-10 18:34:23 +0100

    .

--- NumPy/shape_manipulation.py
+++ NumPy/shape_manipulation.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 from numpy.random import Generator, PCG64
-a = np.ones((2,3), dtype=int)
+a = np.ones((3,4), dtype=int)
+print(a)
+print(a.shape)
 rg = Generator(PCG64())
-a = rg.standard_normal((2,3))
+a = rg.standard_normal((3,4))
 print(a)
 print(a.shape)
 
@@ -26,8 +28,8 @@ a = a.reshape(3,-1)
 print(a)
 
 print('Stacking together different arrays')
-a = np.floor(10*rg.standard_normal((2,2))))
-b = np.floor(10*rg.standard_normal((2,2))))
+a = np.floor(10*rg.standard_normal((2,2)))
+b = np.floor(10*rg.standard_normal((2,2)))
 c = np.vstack((a,b))
 print(c)
 c = np.hstack((a,b))
